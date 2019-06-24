419cdfc340f9e1aafe129b4789e8589337ff983d
Jared Burnham <zlolris97@gmail.com>
2019-06-24 22:34:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ function theBeatlesPlay(musicians, instruments){
     }
   return array
 }
-var i
+
+function johnLennonFacts(facts){
+  var i
 const facts = [
         "He was the last Beatle to learn to drive",
         "He was never a vegetarian",
         "He was a choir boy and boy scout",
         "He hated the sound of his own voice"
       ]  
-function johnLennonFacts(facts){
   var array=[];
   while (i<4){
     array.push(`${facts}!!!`)
